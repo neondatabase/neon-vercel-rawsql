f293f98d3621bba9451af19eb4fee49939f4ae41
George MacKerron <george@mackerron.co.uk>
2023-02-20 14:48:45 +0000

    Further attempts ...

--- api/sites.ts
+++ api/sites.ts
@@ -5,9 +5,38 @@ export const config = {
   regions: ['fra1'],  // fra1 = Frankfurt: pick the Vercel region nearest your Neon DB
 };
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+let pool: Pool | undefined;
+let poolCreatedAt: string | undefined;
+let poolTimeout: ReturnType<typeof setTimeout> | undefined;
+let poolTimeoutResolve: (() => void) | undefined;
 
-export default async (req: Request) => {
+export default async (req: Request, event: any) => {
+  console.log(`Handling request`);
+
+  if (pool) {
+    // tear down the keep-awake mechanism we created last time
+    console.log(`Using pool created at: ${poolCreatedAt}`);
+    clearTimeout(poolTimeout);
+    poolTimeoutResolve!();
+    
+  } else {
+    console.log(`Creating new pool`);
+    pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    poolCreatedAt = new Date().toISOString();
+  };
+
+  // keep the runtime awake until ...
+  event.waitUntil(new Promise<void>(resolve => { poolTimeoutResolve = resolve; }));
+
+  // ... there are no requests for 30s, when we end the pool and let the runtime sleep
+  poolTimeout = setTimeout(async () => {
+    console.log(`Ending pool`);
+    const endingPool = pool;
+    pool = undefined;
+    await endingPool?.end();
+    poolTimeoutResolve!();
+  }, 30000);
+
   const longitude = parseFloat(req.headers.get('x-vercel-ip-longitude') ?? '-122.47');
   const latitude = parseFloat(req.headers.get('x-vercel-ip-latitude') ?? '37.81');
   const { rows: sites } = await pool.query(`
@@ -17,9 +46,10 @@ export default async (req: Request) => {
       location <-> st_makepoint($1, $2) AS distance
     FROM whc_sites_2021
     ORDER BY distance
-    LIMIT 10`, 
+    LIMIT 10`,
     [longitude, latitude]
   );
 
-  return new Response(JSON.stringify({ longitude, latitude, sites }, null, 2));
+  const { totalCount, idleCount, waitingCount } = pool;
+  return new Response(JSON.stringify({poolCreatedAt, totalCount, idleCount, waitingCount, longitude, latitude, sites }, null, 2));
 }
